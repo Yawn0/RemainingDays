e3971d25ce2b05a22d233792166f09aaaa59d894
Yawn0 <77552501+Yawn0@users.noreply.github.com>
2022-02-21 20:18:52 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemainingDays
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime end = new(2022, 6, 3);
+
+            List<DateTime> datesToExclude = new List<DateTime>()
+            {
+                new DateTime(2022,4,5),
+                new DateTime(2022,4,6),
+                new DateTime(2022,6,2)
+            };
+
+            Console.WriteLine($"{ GetSchoolDays(DateTime.Now, end, datesToExclude)} days remaining :D");
+        }
+
+        public static int GetSchoolDays(DateTime current, DateTime end, List<DateTime> datesToExclude)
+        {
+            bool isWorkingDay(int days)
+            {
+                DateTime currentDate = current.AddDays(days);
+                bool isNonWorkingDay =
+                    currentDate.DayOfWeek == DayOfWeek.Saturday ||
+                    currentDate.DayOfWeek == DayOfWeek.Sunday ||
+                    currentDate.DayOfWeek == DayOfWeek.Friday ||
+                    datesToExclude.Exists(Dates => Dates.Date.Equals(currentDate.Date));
+
+                return !isNonWorkingDay;
+            }
+
+            return Enumerable.Range(0, (end - current).Days).Count(isWorkingDay);
+        }
+    }
+}
+
